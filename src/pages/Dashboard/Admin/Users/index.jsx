658036395275fcbcb5effb5d0ad9ba1814a18dd9
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-06-07 22:41:36 +0600

    when show user route set a loading

--- src/pages/Dashboard/Admin/Users/index.jsx
+++ src/pages/Dashboard/Admin/Users/index.jsx
@@ -1,12 +1,20 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
 import Swal from "sweetalert2";
+import LoaderSpinner from "../../../../components/LoaderSpinner";
 
 const Users = () => {
+  const [loading, setLoading] = useState(false);
+
   const [users, setUsers] = useState([]);
   useEffect(() => {
-    axios.get("http://localhost:3001/users").then(({data}) => setUsers(data));
+    setLoading(true);
+    axios.get("http://localhost:3001/users").then(({data}) => {
+      setUsers(data);
+      setLoading(false);
+    });
   }, []);
+  console.log(loading);
   const handleMakeAdmin = (_id, name) => {
     axios.patch(`http://localhost:3001/users/admin/${_id}`).then(({data}) => {
       if (data.modifiedCount) {
@@ -38,52 +46,62 @@ const Users = () => {
   };
   return (
     <div>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-          <thead className="text-xs uppercase text-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="bg-gray-50 px-6 py-3 dark:bg-gray-800">
-                Name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Email
-              </th>
-              <th scope="col" className="bg-gray-50 px-6 py-3 dark:bg-gray-800">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {users &&
-              users.map((user) => (
-                <tr
-                  key={user._id}
-                  className="border-b border-gray-200 dark:border-gray-700">
-                  <th
-                    scope="row"
-                    className="whitespace-nowrap bg-gray-50 px-6 py-4 font-medium text-gray-900 dark:bg-gray-800 dark:text-white">
-                    {user.name}
-                  </th>
-                  <td className="px-6 py-4">{user.email}</td>
-                  <td className="bg-gray-50 px-6 py-4 dark:bg-gray-800">
-                    <button
-                      onClick={() => handleMakeInstructor(user._id, user.name)}
-                      className="btn-sm btn py-1 "
-                      disabled={user?.userType === "instructor"}>
-                      Instructor
-                    </button>
-                    <button
-                      onClick={() => handleMakeAdmin(user._id, user.name)}
-                      className="btn-sm btn py-1"
-                      disabled={user?.userType === "admin"}>
-                      Admin
-                    </button>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
+      {loading ? (
+        <LoaderSpinner />
+      ) : (
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+            <thead className="text-xs uppercase text-gray-700 dark:text-gray-400">
+              <tr>
+                <th
+                  scope="col"
+                  className="bg-gray-50 px-6 py-3 dark:bg-gray-800">
+                  Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Email
+                </th>
+                <th
+                  scope="col"
+                  className="bg-gray-50 px-6 py-3 dark:bg-gray-800">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {users &&
+                users.map((user) => (
+                  <tr
+                    key={user._id}
+                    className="border-b border-gray-200 dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="whitespace-nowrap bg-gray-50 px-6 py-4 font-medium text-gray-900 dark:bg-gray-800 dark:text-white">
+                      {user.name}
+                    </th>
+                    <td className="px-6 py-4">{user.email}</td>
+                    <td className="bg-gray-50 px-6 py-4 dark:bg-gray-800">
+                      <button
+                        onClick={() =>
+                          handleMakeInstructor(user._id, user.name)
+                        }
+                        className="btn-sm btn py-1 "
+                        disabled={user?.userType === "instructor"}>
+                        Instructor
+                      </button>
+                      <button
+                        onClick={() => handleMakeAdmin(user._id, user.name)}
+                        className="btn-sm btn py-1"
+                        disabled={user?.userType === "admin"}>
+                        Admin
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
